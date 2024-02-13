be9059010e15415b8acc3ea5553769338f6311f8
SauravMuke54 <saurav54muke@gmail.com>
2024-02-13 15:41:22 +0530

    Login and home page done with dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
-
-import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Navbar from './Component/Navbar/Navbar';
+import Home from './Component/Home/Home';
+import Login from './Component/Login/Login';
+import Signup from './Component/Signup/Signup';
+import Dashboard from './Component/Dashboard/Dashboard';
+import PrivateRoute from './Helpers/PrivateRoute';
+// import Editor from './Component/Editor/Editor';
 
 function App() {
+  
   return (
-   <div>
-    hi
-   </div>
+    <div style={{backgroundColor:"#1c2333",  background: 'linear-gradient(to right, #8e44ad, #3498db, #00cec9)'}}>
+    <BrowserRouter>
+        <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/signup' element={<Signup/>}></Route>
+      <Route path='/dashboard' element={<PrivateRoute><Dashboard/></PrivateRoute>}></Route>
+      {/* <Route path='/room/:roomId' element={<PrivateRoute><Editor/></PrivateRoute>}></Route> */}
+
+    </Routes>
+    
+    </BrowserRouter>
+    </div>
   );
 }
 
